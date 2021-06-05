f88e27ccf211f212907f4cca1a5dab67f5688339
Nicholas Baudoin <nick@alkalidesigns.com>
2021-06-04 20:59:06 -0500

    fix mobile logo svg

--- components/mobileNav.js
+++ components/mobileNav.js
@@ -5,7 +5,7 @@ const MobileNav = () => {
         <nav>
             <div className="max-w-7xl md:px-0 bg-white flex m-auto justify-between items-center py-7">
                 <div className="">
-                    <a href="/"><img className="w-48" src="/images/ben-logo.svg" /></a>
+                    <a href="/"><img className="w-48" src="/images/ben-logo.png" /></a>
                 </div>
                 <div className="">
                     <MobileDropdown />
