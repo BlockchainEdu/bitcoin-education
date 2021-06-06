3f6f86f875c8d1d7598d58e0b81c7fbbdd658386
Nicholas Baudoin <nick@alkalidesigns.com>
2021-06-05 21:27:39 -0500

    fix advisor alignment issue

--- components/nationalTeamCard.js
+++ components/nationalTeamCard.js
@@ -3,7 +3,7 @@ import MobileDropdown from "./mobileDropdown"
 const NationalTeamCard = (props) => {
     return (
         <div className="text-center m-auto">
-            <img src={props.image} />
+            <img className="m-auto" src={props.image} />
             <h3 className="font-mont font-bold text-xl text-black pt-5">{props.name}</h3>
             <div className="text-bengrey-300">{props.title}</div>
         </div>
