08ef06ebf69ecbf4f37cc75ff7063304bbc17fe3
Nicholas Baudoin <nick@alkalidesigns.com>
2021-06-18 19:45:36 -0500

    slight changes according to feedback

--- components/nationalTeamCard.js
+++ components/nationalTeamCard.js
@@ -13,8 +13,8 @@ class NationalTeamCard extends React.Component {
 
     onClickFilp = () => {
         this.props.setGlobalClick(true);
-        this.setState({toggle: !this.state.toggle})     
-    } 
+        this.setState({ toggle: !this.state.toggle })
+    }
 
     render() {
         return (
@@ -23,7 +23,7 @@ class NationalTeamCard extends React.Component {
                     <div className={"flip-card " + (this.state.toggle && this.props.globalClick ? "hover" : "")}>
                         <div className="flip-card-inner" onClick={() => this.onClickFilp()} >
                             <div className="flip-card-front">
-                                <img className="m-auto" src={this.props.image} filp-card-container="true"/>
+                                <img className="m-auto" src={this.props.image} filp-card-container="true" />
                             </div>
                             <div className="flip-card-back text-black font-mont p-5 text-sm text-left">
                                 {/* <div filp-card-container="true">Ashton Barger is a Past President of the Miami University Blockchain Club and a 2020 Graduate of Miami University of Ohio. He currently works full time as an Account Manager for Zebu Digital, a Crypto and Blockchain Marketing Agency. He has been volunteering as the President of BEN USA since January of 2021 and is loving the progress the team has made since joining BEN. In his free time he enjoys hiking, skiing,</div> */}
@@ -35,11 +35,14 @@ class NationalTeamCard extends React.Component {
                 <h3 className="font-mont font-bold text-xl text-black pt-5">{this.props.name}</h3>
                 <div className="text-bengrey-300 pt-2">{this.props.title}</div>
                 <div className="text-center social-icons pt-3">
-                    { this.props.twitter && 
+                    {this.props.twitter &&
                         <a href={this.props.twitter} target="_blank"><img className="m-auto" src="/images/twitter-icon.png" /></a>
                     }
-                    { this.props.linkedin && 
+                    {this.props.linkedin &&
                         <a href={this.props.linkedin} target="_blank"><img className="m-auto" src="/images/linkedin-icon.png" /></a>
+                    }
+                    {this.props.linkedin &&
+                        <a href={this.props.email} target="_blank"><img className="m-auto" src="/images/mail-icon.png" /></a>
                     }
                 </div>
             </div>
@@ -49,9 +52,9 @@ class NationalTeamCard extends React.Component {
 
 // const NationalTeamCard = (props) => {
 
-    
-    
-    
+
+
+
 // }
 
 export default NationalTeamCard
