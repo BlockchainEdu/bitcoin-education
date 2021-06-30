3c0bb86659f6dea32af9504086fc8ea3f70b106a
Nicholas Baudoin <nick@alkalidesigns.com>
2021-06-30 01:04:44 -0500

    remove duplicate about and update logo

--- components/mobileDropdown.js
+++ components/mobileDropdown.js
@@ -50,9 +50,6 @@ export default function MobileDropdown(props) {
               <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                 <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
                   <ul className="space-y-4 font-mont text-chablack-300 hover:text-chablack-500">
-                  <li>
-                      <a href="/about">About</a>
-                    </li>
                     <li>
                       <a target="_blank" href="https://portal.blockchainedu.org/all-courses">Learn</a>
                     </li>
