a1ea186f171baa147198013c06d4ff6ffb265cba
Nicholas Baudoin <nick@alkalidesigns.com>
2021-06-30 23:04:36 -0500

    teachers section

--- pages/learn.js
+++ pages/learn.js
@@ -39,7 +39,7 @@ export default function Learn() {
                         <div className="pb-3">How to set up Metamask</div>
                         <div className="pb-3">How to start a Blockchain club</div>
                     </div>
-                    <button className="mt-3 border font-bold text-xl px-8 rounded-full py-4 transition duration-500 hover:text-white text-benblack-500 hover:bg-bengrey-300">Sign up to learn</button>
+                    <button className="mt-5 border font-bold text-xl px-8 rounded-full py-4 transition duration-500 hover:text-white text-benblack-500 hover:bg-bengrey-300">Sign up to learn</button>
                 </div>
                 <div className="w-3/5">
                     <div className="flex justify-end">
@@ -49,6 +49,36 @@ export default function Learn() {
                             height="581"
                             quality="100"
                         />
+                    </div>
+                </div>
+            </section>
+            <section className="max-w-7xl m-auto py-24 pb-14 px-7">
+                <div className="flex flex-col md:flex-row justify-between">
+                    <div className="w-3/6">
+                        <img className="mx-auto md:mx-0" src="/images/learn-teachers-illustration.png" />
+                    </div>
+                    <div className="w-3/6">
+                        <div className="font-mont text-center md:text-left text-xs uppercase pb-7">
+                            Share what you know with the world
+                        </div>
+                        <h2 className="font-black text-center md:text-left text-4xl md:text-5xl text-black pb-10">
+                            The 10:10 Plan
+                        </h2>
+                        <div className="flex items-center gap-x-0 md:gap-x-10 justify-center">
+                            <div className="border-r-none md:border-r md:border-4 md:border-benorange-500  h-24">
+
+                            </div>
+                            <div>
+                                <p className="font-mont text-center md:text-left mx-auto md:mx-0 text-xl md:text-2xl font-bold max-w-sm">
+                                    We invite anyone interested in creating a course on BEN Learn to fill out the form below and we will get back to you as soon as we can with whether we can support your course.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="m-auto flex content-center justify-center md:justify-start">
+                            <button className="mb-20 md:mb-0 bg-benorange-500 hover:bg-bengrey-300 shadow-button transition duration-500 text-white font-bold text-xl px-12 rounded-full py-4 mt-10">
+                                Learn more
+                            </button>
+                        </div>
                     </div>
                 </div>
             </section>
