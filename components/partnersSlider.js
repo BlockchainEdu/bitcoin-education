690be438e3b3191f1db4ee96be7f7198265d72af
Nicholas Baudoin <nick@alkalidesigns.com>
2021-07-04 23:07:24 -0500

    add standard slick responsiveness

--- components/partnersSlider.js
+++ components/partnersSlider.js
@@ -9,7 +9,32 @@ const PartnerSlider = ({data, title}) => {
     autoplay: true,
     speed: 40000,
     autoplaySpeed: 0,
-    cssEase: "linear"
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   };
 
   return (
