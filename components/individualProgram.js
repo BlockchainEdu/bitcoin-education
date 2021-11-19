e6e2da645a4dc2244374c1d988b57d1aee3cf9e9
Nicholas Baudoin <nick@alkalidesigns.com>
2021-11-18 21:48:52 -0600

    connect more Monday API endpoints

--- components/individualProgram.js
+++ components/individualProgram.js
@@ -70,14 +70,25 @@ export default function IndividualProgram() {
                 <h1 className="text-4xl md:text-6xl text-center lg:text-left font-black text-black max-w-5xl pt-10 leading-snug">
                   {global.name}
                 </h1>
-                <p className="text-black text-md pt-10 max-w-lg m-auto lg:m-0 text-center lg:text-left font-medium">
-                  {JSON.parse(global.column_values[1].value)}
-                </p>
-                <a target="_blank" href={JSON.parse(global.column_values[5].value)}>
-                  <button className="mb-20 md:mb-0 bg-benorange-500 hover:bg-bengrey-300 shadow-button transition duration-500 text-white font-bold text-xl px-12 rounded-full py-4 mt-10">
-                    {JSON.parse(global.column_values[4].value)}
-                  </button>
-                </a>
+                <ul className="space-y-10 text-black text-md pt-10 max-w-lg m-auto lg:m-0 text-center lg:text-left font-medium">
+                  <li>{JSON.parse(global.column_values[1].value)}</li>
+                  <li>{JSON.parse(global.column_values[7].value)}</li>
+                  <li>{JSON.parse(global.column_values[8].value)}</li>
+                  <li>{JSON.parse(global.column_values[9].value)}</li>
+                  <li>{JSON.parse(global.column_values[10].value)}</li>
+                </ul>
+                <div className="flex gap-x-10">
+                  <a target="_blank" href={JSON.parse(global.column_values[4].value)}>
+                    <button className="mb-20 md:mb-0 bg-benorange-500 hover:bg-bengrey-300 shadow-button transition duration-500 text-white font-bold text-xl px-12 rounded-full py-4 mt-10">
+                      {JSON.parse(global.column_values[3].value)}
+                    </button>
+                  </a>
+                  <a target="_blank" href={JSON.parse(global.column_values[4].value)}>
+                    <button className="mb-20 md:mb-0 transition duration-500 text-benorange-500 font-bold text-xl px-12 rounded-full py-4 mt-10">
+                      {JSON.parse(global.column_values[3].value)}
+                    </button>
+                  </a>
+                </div>
               </div>
               <div className="w-full lg:w-6/12 m-auto pt-14 pb-0 lg:pb-24">
                 <img className="m-auto" src={global.assets.length > 0 ? global.assets[0]?.public_url : ""} />
