595834c20ab53d9e1062ee01abeecc217f2f652d
Nicholas Baudoin <nick@alkalidesigns.com>
2021-11-18 22:04:22 -0600

    fix button issue

--- components/individualProgram.js
+++ components/individualProgram.js
@@ -78,14 +78,14 @@ export default function IndividualProgram() {
                   <li>{JSON.parse(global.column_values[10].value)}</li>
                 </ul>
                 <div className="flex gap-x-10 flex-col lg:flex-row mx-auto">
-                  <a target="_blank" href={JSON.parse(global.column_values[4].value)}>
+                  <a className="mx-auto lg:mx-0" target="_blank" href={JSON.parse(global.column_values[4].value)}>
                     <button className="mb-20 md:mb-0 bg-benorange-500 hover:bg-bengrey-300 shadow-button transition duration-500 text-white font-bold text-xl px-12 rounded-full py-4 mt-10">
                       {JSON.parse(global.column_values[3].value)}
                     </button>
                   </a>
-                  <a target="_blank" href={JSON.parse(global.column_values[4].value)}>
+                  <a className="mx-auto lg:mx-0" target="_blank" href={JSON.parse(global.column_values[6].value)}>
                     <button className="mb-20 md:mb-0 transition duration-500 text-benorange-500 font-bold text-xl px-12 rounded-full py-4 mt-10">
-                      {JSON.parse(global.column_values[3].value)}
+                      {JSON.parse(global.column_values[5].value)}
                     </button>
                   </a>
                 </div>
