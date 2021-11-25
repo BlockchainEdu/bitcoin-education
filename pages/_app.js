f484e5704a3f955096faff7134d5d4d17ffdaaec
Nicholas Baudoin <nick@alkalidesigns.com>
2021-11-25 16:51:20 -0600

    add gtm tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
 import 'tailwindcss/tailwind.css'
 import '../public/styles/global.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        dangerouslySetInnerHTML={{
+          __html: `
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-213540060-2"></script>
+          <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-213540060-2');
+  `,
+        }}
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
