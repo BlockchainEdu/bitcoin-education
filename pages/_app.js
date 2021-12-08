6eda30cb9a1558d23e02e143ada54eb9ccbaa1db
Nicholas Baudoin <nick@alkalidesigns.com>
2021-12-08 11:04:30 -0600

    fix font issue

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,21 @@ import 'tailwindcss/tailwind.css'
 import { useEffect } from 'react';
 import '../public/styles/global.css'
 import TagManager from 'react-gtm-module';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-213540060-2' });
   }, []);
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
