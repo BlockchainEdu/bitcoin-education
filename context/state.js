8fdc7cd7d8876551ddeafe2cdeb312099d70b39c
Arthur Gloer <arthurgloer@gmail.com>
2022-04-08 18:45:52 -0400

    Forgot to add in the app context.

---
+++ context/state.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+export function AppWrapper({ children }) {
+  const [ sharedState, setSharedState ] = useState({
+    latitude: 38.5,
+    longitude: -99,
+    zoom: 3,
+  });
+
+  return (
+    <AppContext.Provider value={{sharedState, setSharedState}}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
