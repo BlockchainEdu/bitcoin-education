3907f19756cc55f73a515e63d2ca1783b2e32f2c
baudoinalkali <79876365+baudoinalkali@users.noreply.github.com>
2022-04-14 20:40:37 -0500

    change zoom default

--- context/state.js
+++ context/state.js
@@ -6,7 +6,7 @@ export function AppWrapper({ children }) {
   const [ sharedState, setSharedState ] = useState({
     latitude: 30.576450,
     longitude: -10.642023,
-    zoom: 3,
+    zoom: 5,
     scrollY: 0,
   });
 
