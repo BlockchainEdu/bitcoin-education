b38106fe6fa20568ae02636ca636c573006f0575
harisco7 <harissmajlagic3990@gmail.com>
2022-07-03 00:51:36 +0000

    Update Subscribe function

--- components/subscribe.js
+++ components/subscribe.js
@@ -1,78 +1,67 @@
 import React from "react";
-import Mailchimp from 'react-mailchimp-form'
+import Mailchimp from "react-mailchimp-form";
 
 export default function Subscribe() {
-    const [showModal, setShowModal] = React.useState(false);
-    return (
-        <>
-            <button
-                className="bg-benorange-500 hover:bg-bengrey-300 transition duration-500 shadow-button text-white font-bold text-xl px-16 rounded-full py-4"
-                type="button"
-                onClick={() => setShowModal(true)}
-            >
-                Subscribe
+  const [showModal, setShowModal] = React.useState(false);
+  return (
+    <>
+      <button
+        className="bg-benorange-500 hover:bg-bengrey-300 transition duration-500 shadow-button text-white font-bold text-xl px-16 rounded-full py-4"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Subscribe
       </button>
-            {showModal ? (
-                <>
-                    <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-3 md:mx-0"
-                    >
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                            {/*content*/}
-                            <div className="border-0 donation-modal rounded-2xl shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                {/*header*/}
-                                <div className="pt-10 flex items-start justify-between p-5 rounded-t">
-                                    <h3 className="text-3xl font-mont font-bold">
-                                        Subscribe
-                  </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-transparent border-0 hover:bg-bengray-300 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="bg-gray rounded-full h-3 w-3 text-black">
-                                            ×
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-3 md:mx-0">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 donation-modal rounded-2xl shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="pt-10 flex items-start justify-between p-5 rounded-t">
+                  <h3 className="text-3xl font-mont font-bold">Subscribe</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 hover:bg-bengray-300 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-gray rounded-full h-3 w-3 text-black">
+                      ×
                     </span>
-                                    </button>
-                                </div>
-                                {/*body*/}
-                                <div className="relative p-6 pb-10 md:pb-0 flex-auto">
-                                    <div>
-                                        <Mailchimp
-                                            action='https://blockchainedu.us4.list-manage.com/subscribe/post?u=8f05e1771877392fa3d41df41&amp;id=a53b080887'
-                                            fields={[
-                                                {
-                                                    name: 'EMAIL',
-                                                    placeholder: 'Email',
-                                                    type: 'email',
-                                                    required: true
-                                                },
-                                                {
-                                                    name: 'SCHOOL',
-                                                    placeholder: 'School',
-                                                    type: 'text',
-                                                    required: true
-                                                }
-                                            ]}
-                                            messages = {
-                                                {
-                                                  sending: "Sending...",
-                                                  success: "Thank you for subscribing!",
-                                                  error: "An unexpected internal error has occurred.",
-                                                  empty: "You must write an e-mail.",
-                                                  duplicate: "Too many subscribe attempts for this email address",
-                                                  button: "Subscribe"
-                                                }
-                                              }
-                                            className="subscribe-form text-center "
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="button-overlay opacity-75 fixed inset-0 z-40 bg-black"></div>
-                </>
-            ) : null}
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 pb-10 md:pb-0 flex-auto">
+                  <div>
+                    <Mailchimp
+                      action="https://blockchainedu.us4.list-manage.com/subscribe/post?u=8f05e1771877392fa3d41df41&amp;id=a53b080887"
+                      fields={[
+                        {
+                          name: "EMAIL",
+                          placeholder: "Email",
+                          type: "email",
+                          required: true,
+                        },
+                      ]}
+                      messages={{
+                        sending: "Sending...",
+                        success: "Thank you for subscribing!",
+                        error: "An unexpected internal error has occurred.",
+                        empty: "You must write an e-mail.",
+                        duplicate:
+                          "Too many subscribe attempts for this email address",
+                        button: "Subscribe",
+                      }}
+                      className="subscribe-form text-center "
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="button-overlay opacity-75 fixed inset-0 z-40 bg-black"></div>
         </>
-    );
+      ) : null}
+    </>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
-import 'tailwindcss/tailwind.css'
-import { useEffect } from 'react';
-import { AppWrapper } from '../context/state'; // import based on where you put it
-import '../public/styles/global.css'
-import TagManager from 'react-gtm-module';
-import Head from 'next/head';
+import "tailwindcss/tailwind.css";
+import { useEffect, useState } from "react";
+import { AppWrapper } from "../context/state"; // import based on where you put it
+import "../public/styles/global.css";
+import TagManager from "react-gtm-module";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
+  const [app, setApp] = useState(false);
   useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-213540060-2' });
+    TagManager.initialize({ gtmId: "GTM-213540060-2" });
+    if (!window.localStorage.getItem("subscribed")) {
+      setApp(true);
+    }
   }, []);
   return (
     <AppWrapper>
@@ -31,12 +35,81 @@ function MyApp({ Component, pageProps }) {
             }}
           />
           <link rel="shortcut icon" href="" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
-        <Component {...pageProps} />
+        {app ? (
+          <div id="revue-embed">
+            <form
+              action="https://www.getrevue.co/profile/blockchainedu/add_subscriber"
+              method="post"
+              id="revue-form"
+              name="revue-form"
+              target="_blank"
+            >
+              <div class="revue-form-group">
+                <label for="member_email">Email address</label>
+                <input
+                  class="revue-form-field"
+                  placeholder="Your email address..."
+                  type="email"
+                  name="member[email]"
+                  id="member_email"
+                />
+              </div>
+              <div class="revue-form-group">
+                <label for="member_first_name">
+                  First name <span class="optional">(Optional)</span>
+                </label>
+                <input
+                  class="revue-form-field"
+                  placeholder="First name... (Optional)"
+                  type="text"
+                  name="member[first_name]"
+                  id="member_first_name"
+                />
+              </div>
+              <div class="revue-form-group">
+                <label for="member_last_name">
+                  Last name <span class="optional">(Optional)</span>
+                </label>
+                <input
+                  class="revue-form-field"
+                  placeholder="Last name... (Optional)"
+                  type="text"
+                  name="member[last_name]"
+                  id="member_last_name"
+                />
+              </div>
+              <div class="revue-form-actions">
+                <input
+                  type="submit"
+                  value="Subscribe"
+                  name="member[subscribe]"
+                  id="member_submit"
+                />
+              </div>
+              <div class="revue-form-footer">
+                By subscribing, you agree with Revue’s{" "}
+                <a target="_blank" href="https://www.getrevue.co/terms">
+                  Terms of Service
+                </a>{" "}
+                and{" "}
+                <a target="_blank" href="https://www.getrevue.co/privacy">
+                  Privacy Policy
+                </a>
+                .
+              </div>
+            </form>
+          </div>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </div>
     </AppWrapper>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
