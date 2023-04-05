043fd193150a4735d74472bf48fa9c106878fed9
Erick Pinos <erick@erickpinos.com>
2023-04-04 21:12:50 -0400

    added BEN Beats and meme

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -2,6 +2,7 @@
 import Header from "../components/header";
 import StandardButton from "../components/standardButton";
 import Head from "next/head"
+import Image from 'next/image'
 
 export default function ThankYou() {
   return (
@@ -13,7 +14,7 @@ export default function ThankYou() {
       <section className="flex mt-24 lg:mt-auto mb-24 lg:mb-auto px-7 lg:px-0">
         <div className="max-w-lg m-auto py-4">
           <h1 className="mb-10 text-center font-mont text-4xl lg:text-5xl font-black">Thank You for Subscribing!</h1>
-          <h2 className="mb-6 text-center text-xl lg:text-2xl">Welcome to BEN’s Newsletter!</h2>
+          <h2 className="mb-6 text-center text-xl lg:text-2xl">Welcome to BEN Beats!</h2>
           <div className="mb-6">Hey, BENsters! Excited to hit the road with ya!</div>
           <div className="mb-6">To-Do List:</div>
           <div className="mb-6">
@@ -28,7 +29,7 @@ export default function ThankYou() {
           <div className="mb-6">Keep your eyes peeled 👀, don't wanna miss out on this!</div>
           <div className="mb-6">Do you have a minute or two to check your email? We just sent you something! 😊</div>
           <div className="mb-6">To the moon and beyond 🚀🌙,<br/>- BEN Team</div>
-
+          <img width="250px" height="250px" src="/images/dogecoin-to-the-moon.gif" loop />
         </div>
       </section>
       <Footer />
