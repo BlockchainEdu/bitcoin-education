69b69f8c767d7b70c4d63b04f256f3659ea944c1
Erick Pinos <erick@erickpinos.com>
2023-05-23 18:51:44 -0400

    added Facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -18,25 +18,54 @@ function MyApp({ Component, pageProps }) {
 
 return (
     <AppWrapper>
-      <div>
-        <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=UA-213540060-2`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-213540060-2', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
+        <div>
+            <Head>
+
+                {/* Global Site Tag (gtag.js) - Google Analytics */}
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=UA-213540060-2`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'UA-213540060-2', {
+                        page_path: window.location.pathname,
+                        });
+                        `,
+                    }}
+                />
+
+                {/* Facebook Pixel Code */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        !function(f,b,e,v,n,t,s){
+                            if(f.fbq)return;
+                            n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;
+                            n.push=n;
+                            n.loaded=!0;
+                            n.version='2.0';
+                            n.queue=[];
+                            t=b.createElement(e);
+                            t.async=!0;
+                            t.src=v;
+                            s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)
+                          }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+                          fbq('init', '647331727208880');
+                          fbq('track', 'PageView');
+                          `,
+                    }}
+                />
+                <noscript>
+                      <img height="1" width="1" src="https://www.facebook.com/tr?id=647331727208880&ev=PageView&noscript=1"/>
+                </noscript>
+
           <link rel="shortcut icon" href="" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"></link>
