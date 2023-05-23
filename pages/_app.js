f6c40a1000c022550934ad51b7f3856e5b84d0d5
Erick Pinos <erick@erickpinos.com>
2023-05-22 21:49:40 -0400

    removed git

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { AppWrapper } from '../context/state'; // import based on where you put 
 import '../public/styles/global.css'
 import TagManager from 'react-gtm-module';
 import Head from 'next/head';
-git
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-213540060-2' });
