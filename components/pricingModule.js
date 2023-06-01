c2912994246ee7a356a41c852629b6e95a45b8d1
Erick Pinos <erick@erickpinos.com>
2023-06-01 00:15:15 -0400

    updated free trial language

--- components/pricingModule.js
+++ components/pricingModule.js
@@ -1,6 +1,6 @@
-﻿import { useState } from 'react'
+﻿import { useState } from 'react';
 
-export default function PopupMOdule() {
+export default function PopupModule() {
   const [activeTab, setActiveTab] = useState("annual");
 
   const handleTabClick = (tab) => {
@@ -9,85 +9,94 @@ export default function PopupMOdule() {
 
   return (
     <div className="col-span-1 flex justify-center">
-      <div className=" w-11/12 lg:w-1/2 flex flex-col p-4 rounded-lg border border-gray-300 p-8 shadow-lg">
+      <div className="w-11/12 lg:w-1/2 flex flex-col p-4 rounded-lg border border-gray-300 p-8 shadow-lg">
         <div className="flex justify-center">
-            <div className="flex w-full max-w-sm space-x-1 bg-gray-300 rounded-xl p-1 items-stretch mb-4 justify-center">
-              <button
-                className={`px-4 py-2 rounded-md w-full ${
-                  activeTab === "annual" ? "bg-white text-black" : "bg-gray-300 text-black"
-                }`}
-                onClick={() => handleTabClick("annual")}
-              >
-                Annual (20% off!)
-              </button>
-              <button
-                className={`px-4 py-2 rounded-md w-full ${
-                  activeTab === "monthly" ? "bg-white  text-black" : "bg-gray-300 text-black"
-                }`}
-                onClick={() => handleTabClick("monthly")}
-              >
-                Monthly
-              </button>
-    </div>
+          <div className="flex w-full max-w-sm space-x-1 bg-gray-300 rounded-xl p-1 items-stretch mb-4 justify-center">
+            <button
+              className={`px-4 py-2 rounded-md w-full ${activeTab === "annual" ? "bg-white text-black" : "bg-gray-300 text-black"}`}
+              onClick={() => handleTabClick("annual")}
+            >
+              Annual (20% off!)
+            </button>
+            <button
+              className={`px-4 py-2 rounded-md w-full ${activeTab === "monthly" ? "bg-white text-black" : "bg-gray-300 text-black"}`}
+              onClick={() => handleTabClick("monthly")}
+            >
+              Monthly
+            </button>
+          </div>
         </div>
         <div>
           {activeTab === "annual" && (
-              <div>
-                <div className="text-center">  
-                    <div className="mb-1"><span className="text-8xl font-bold">$16</span><span> / mo</span></div>
-                    <div><i>$192 billed annually</i></div>
+            <div>
+              <div className="text-center">
+                <div className="mb-1">
+                  <span className="text-8xl font-bold">$16</span>
+                  <span> / mo</span>
                 </div>
-        <div className="flex justify-center mt-4 mb-4">
-          <a href="https://learn.blockchainedu.org/membership-account/membership-checkout/?level=4" className="px-4 py-2 bg-benorange-500  text-white rounded-md">
-            Get A 7 Day Free Trial!
-          </a>
-        </div>
-                <ul className="space-y-2">
-                  <li className="flex justify-between">
-                    <span>✅ Deep dive analyses to potential 1000x projects</span>
-                  </li>
-                  <li className="flex justify-between">
-                    <span>✅ Access to a premium community of investors, builders, and founders</span>
-                  </li>
-                  <li className="flex justify-between">
-                    <span>✅ Direct access to team members for questions and answers</span>
-                  </li>
-                  <li className="flex justify-between">
-                    <span>✅ Access to networking opportunities with other founders</span>
-                  </li>
-                </ul>
+                {/*
+                <div>
+                  <i>$16 / mo</i>
+                </div>
+                */}
               </div>
+              <div className="flex justify-center mt-4 mb-4">
+                <a href="https://learn.blockchainedu.org/membership-account/membership-checkout/?level=4" className="px-4 py-2 bg-black text-white rounded-md">
+                  Get a 7 Day Free Trial!
+                </a>
+              </div>
+              <ul className="space-y-2">
+                <li className="flex justify-between">
+                  <span>✅ Deep dive analyses to potential 1000x projects</span>
+                </li>
+                <li className="flex justify-between">
+                  <span>✅ Access to a premium community of investors, builders, and founders</span>
+                </li>
+                <li className="flex justify-between">
+                  <span>✅ Direct access to team members for questions and answers</span>
+                </li>
+                <li className="flex justify-between">
+                  <span>✅ Access to networking opportunities with other founders</span>
+                </li>
+              </ul>
+            </div>
           )}
           {activeTab === "monthly" && (
             <div>
-                <div className="text-center">  
-                    <div className="mb-1"><span className="text-8xl font-bold">$20</span><span> / mo</span></div>
-                  <div><i>$20 billed monthly</i></div>
+              <div className="text-center">
+                <div className="mb-1">
+                  <span className="text-8xl font-bold">$20</span>
+                  <span> / mo</span>
                 </div>
-        <div className="flex justify-center mt-4 mb-4">
-          <a href="https://learn.blockchainedu.org/membership-account/membership-checkout/?level=3" className="px-4 py-2 bg-benorange-500 text-white rounded-md">
-            Get A 7 Day Free Trial!
-          </a>
-        </div>
-                <ul className="space-y-2">
-                  <li className="flex justify-between">
-                    <span>✅ Deep dive analyses to potential 1000x projects</span>
-                  </li>
-                  <li className="flex justify-between">
-                    <span>✅ Access to a premium community of investors, builders, and founders</span>
-                  </li>
-                  <li className="flex justify-between">
-                    <span>✅ Direct access to team members for questions and answers</span>
-                  </li>
-                  <li className="flex justify-between">
-                    <span>✅ Access to networking opportunities with other founders</span>
-                  </li>
-                </ul>
+                {/*
+                <div>
+                  <i>$16 / mo</i>
+                </div>
+                */}
+              </div>
+              <div className="flex justify-center mt-4 mb-4">
+                <a href="https://learn.blockchainedu.org/membership-account/membership-checkout/?level=3" className="px-4 py-2 bg-black text-white rounded-md">
+                  Get a 7 Day Free Trial!
+                </a>
+              </div>
+              <ul className="space-y-2">
+                <li className="flex justify-between">
+                  <span>✅ Deep dive analyses to potential 1000x projects</span>
+                </li>
+                <li className="flex justify-between">
+                  <span>✅ Access to a premium community of investors, builders, and founders</span>
+                </li>
+                <li className="flex justify-between">
+                  <span>✅ Direct access to team members for questions and answers</span>
+                </li>
+                <li className="flex justify-between">
+                  <span>✅ Access to networking opportunities with other founders</span>
+                </li>
+              </ul>
             </div>
           )}
         </div>
       </div>
     </div>
   );
-
 }
--- pages/join.js
+++ pages/join.js
@@ -1,131 +1,127 @@
 import Footer from '../components/footer';
 import Header from '../components/header';
-import Head from "next/head"
+import Head from "next/head";
 import PricingModule from '../components/pricingModule';
 
 export default function Contact() {
-    return (
-        <div id="join-page">
-            <Header />
-            <Head>
-                <title>Join | Blockchain Education Network</title>
-            </Head>
+  return (
+    <div id="join-page">
+      <Header />
+      <Head>
+        <title>Join | Blockchain Education Network</title>
+      </Head>
 
-            <section className="py-20 bg-black text-white mt-5">
-                <div className="container mx-auto px-4">
+      <section className="py-20 bg-black text-white mt-5">
+        <div className="container mx-auto px-4">
+          <div className="max-w-3xl mx-auto text-center">
+            <img src="/images/ben_hat_white.png" alt="Image" className="mx-auto mb-4 w-28" />
+            <h1 className="text-4xl font-bold mb-6">Join BEN Learn</h1>
+            <p className="text-lg mb-8">Get exclusive insights, analysis, and tips delivered to your inbox every week.</p>
+            <a href="#pricingmodule" className="bg-white text-black font-bold py-3 px-8 rounded-full shadow-lg uppercase tracking-wide hover:bg-gray-500" onClick={(e) => {
+              e.preventDefault();
+              const section = document.getElementById("pricingmodule");
+              const offsetTop = section.offsetTop - 120;
+              window.scrollTo({ top: offsetTop, behavior: "smooth" });
+            }}>Join Now</a>
+          </div>
+        </div>
+      </section>
 
-                    <div className="max-w-3xl mx-auto text-center">
-                        <img src="/images/ben_hat_white.png" alt="Image" className="mx-auto mb-4 w-28" />
-                        <h1 className="text-4xl font-bold mb-6">Join BEN Learn</h1>
-                        <p className="text-lg mb-8">Get exclusive insights, analysis, and tips delivered to your inbox every week.</p>
-                        <a href="#pricingmodule" className="bg-white text-black font-bold py-3 px-8 rounded-full shadow-lg uppercase tracking-wide hover:bg-gray-500" onClick={(e) => {
-                            e.preventDefault();
-                            const section = document.getElementById("pricingmodule");
-                            const offsetTop = section.offsetTop - 120;
-                            window.scrollTo({ top: offsetTop, behavior: "smooth" });
-                          }}>Join Now</a>
-                    </div>
-                </div>
-            </section>
-             <section id="benefits" className="py-16">
-                <div className="container mx-auto px-4">
+      <section id="benefits" className="py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 text-center">Weekly Market Reports</h2>
+          <a href="#pricingmodule" onClick={(e) => {
+            e.preventDefault();
+            const section = document.getElementById("pricingmodule");
+            const offsetTop = section.offsetTop - 120;
+            window.scrollTo({ top: offsetTop, behavior: "smooth" });
+          }}>
+            <img src="/images/join-desenho4_copy.avif" alt="Image" className="mx-auto mb-4" />
+          </a>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="bg-white rounded-lg shadow p-6">
+              <h3 className="text-xl font-bold mb-4">Exclusive Insights</h3>
+              <p>Get access to exclusive insights and analysis from industry experts.</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <h3 className="text-xl font-bold mb-4">Actionable Tips</h3>
+              <p>Receive actionable tips and strategies to enhance your skills and stay ahead.</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <h3 className="text-xl font-bold mb-4">In-Depth Research</h3>
+              <p>Benefit from in-depth research and reports on the latest trends and opportunities.</p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-                    <h2 className="text-3xl font-bold mb-8 text-center">Weekly Market Reports</h2>
-<a href="#pricingmodule"  onClick={(e) => {
-                            e.preventDefault();
-                            const section = document.getElementById("pricingmodule");
-                            const offsetTop = section.offsetTop - 120;
-                            window.scrollTo({ top: offsetTop, behavior: "smooth" });
-                          }}>
-                        <img src="/images/join-desenho4_copy.avif" alt="Image" className="mx-auto mb-4" />
-                    </a>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                        <div className="bg-white rounded-lg shadow p-6">
-                            <h3 className="text-xl font-bold mb-4">Exclusive Insights</h3>
-                            <p>Get access to exclusive insights and analysis from industry experts.</p>
-                        </div>
-                        <div className="bg-white rounded-lg shadow p-6">
-                            <h3 className="text-xl font-bold mb-4">Actionable Tips</h3>
-                            <p>Receive actionable tips and strategies to enhance your skills and stay ahead.</p>
-                        </div>
-                        <div className="bg-white rounded-lg shadow p-6">
-                            <h3 className="text-xl font-bold mb-4">In-Depth Research</h3>
-                            <p>Benefit from in-depth research and reports on the latest trends and opportunities.</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+      <section id="benefits" className="py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 text-center">Member Only Discord</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="bg-white rounded-lg shadow p-6">
+              <h3 className="text-xl font-bold mb-4">Online/Offline Community</h3>
+              <p>Join online discussions and get answers to your burning market questions.</p>
+              <p>- Get event promo codes, and meet up with like-minded individuals.</p>
+            </div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <h3 className="text-xl font-bold mb-4">Job Matching and Investments</h3>
+              <ul>
+                <li>- Share job listings and get warm intros to in-demand positions.</li>
+                <li>- If you're working on a startup, meet co-founders and investors.'</li>
+              </ul>
+            </div>
+            <a href="#pricingmodule" onClick={(e) => {
+              e.preventDefault();
+              const section = document.getElementById("pricingmodule");
+              const offsetTop = section.offsetTop - 120;
+              window.scrollTo({ top: offsetTop, behavior: "smooth" });
+            }}>
+              <img src="/images/join-gif.avif" alt="Image" className="mx-auto mb-4" />
+            </a>
+          </div>
+        </div>
+      </section>
 
+      <section id="benefits" className="py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 text-center">Blockchain Courses</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+            <div className="bg-white rounded-lg shadow p-6">
+              <a href="#pricingmodule" onClick={(e) => {
+                e.preventDefault();
+                const section = document.getElementById("pricingmodule");
+                const offsetTop = section.offsetTop - 120;
+                window.scrollTo({ top: offsetTop, behavior: "smooth" });
+              }}>
+                <img src="/images/join-gif1.avif" alt="Image" className="mx-auto mb-4" />
+              </a>
+            </div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <h3 className="text-xl font-bold mb-4">On-demand video coursess</h3>
+              <ul>
+                <li>- Bitcoin & Basics</li>
+                <li>- Blockchain Development</li>
+                <li>- Blockchain Fundamentals</li>
+                <li>- Buying Land in the Metaverse</li>
+                <li>- Crypto Investing & Marketing Basics</li>
+                <li>- DeFi Basics</li>
+                <li>- Ethereum & Smart Contracts Basics</li>
+                <li>- Intro to The Sandbox</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
 
-            <section id="benefits" className="py-16">
-                <div className="container mx-auto px-4">
-                    <h2 className="text-3xl font-bold mb-8 text-center">Member Only Discord</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                        <div className="bg-white rounded-lg shadow p-6">
-                            <h3 className="text-xl font-bold mb-4">Online/Offline Community</h3>
-                            <p>Join online discussions and get answers to your burning market questions.</p>
-                            <p>- Get event promo codes, and meet up with like-minded individuals.</p>
-                        </div>
-                        <div className="bg-white rounded-lg shadow p-6">
-                            <h3 className="text-xl font-bold mb-4">Job Matching and Investments</h3>
-                            <ul>
-                            <p>-Share job listings and get warm intros to in-demand positions.</p>
-                            <p>-If you're working on a startup, meet co-founders and investors.'</p>
-                            </ul>
-                        </div>
-<a href="#pricingmodule"  onClick={(e) => {
-                            e.preventDefault();
-                            const section = document.getElementById("pricingmodule");
-                            const offsetTop = section.offsetTop - 120;
-                            window.scrollTo({ top: offsetTop, behavior: "smooth" });
-                          }}>
-                    <img src="/images/join-gif.avif" alt="Image" className="mx-auto mb-4" />
-                    </a>
-                    </div>
-                </div>
-            </section>
+      <section className="py-16">
+        <div id="pricingmodule" className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 text-center">Ready?</h2>
+        </div>
+        <PricingModule />
+      </section>
 
-
-            <section id="benefits" className="py-16">
-                <div className="container mx-auto px-4">
-                    <h2 className="text-3xl font-bold mb-8 text-center">Blockchain Courses</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
-                        <div className="bg-white rounded-lg shadow p-6">
-<a href="#pricingmodule"  onClick={(e) => {
-                            e.preventDefault();
-                            const section = document.getElementById("pricingmodule");
-                            const offsetTop = section.offsetTop - 120;
-                            window.scrollTo({ top: offsetTop, behavior: "smooth" });
-                          }}>
-                    <img src="/images/join-gif1.avif" alt="Image" className="mx-auto mb-4" />
-                    </a>
-                        </div>
-                        <div className="bg-white rounded-lg shadow p-6">
-                            <h3 className="text-xl font-bold mb-4">On-demand video coursess</h3>
-                        <ul>
-                            <li>- Bitcoin & Basics</li>
-                            <li>- Blockchain Development</li>
-                            <li>- Blockchain Fundamentals</li>
-                            <li>- Buying Land in the Metaverse</li>
-                            <li>- Crypto Investing & Marketing Basics</li>
-                            <li>- DeFi Basics</li>
-                            <li>- Ethereum & Smart Contracts Basics</li>
-                            <li>- Intro to The Sandbox</li>
-                        </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="py-16">
-                <div id="pricingmodule" className="container mx-auto px-4">
-                    <h2 className="text-3xl font-bold mb-8 text-center">Ready?</h2>
-                </div>
-                <PricingModule />
-
-            </section>
-
-    {/*
+      {/*
             <section className="py-16 bg-gray-200">
                 <div className="container mx-auto px-4">
                     <h2 className="text-3xl font-bold mb-8 text-center">What Our Subscribers Say</h2>
@@ -136,9 +132,9 @@ export default function Contact() {
                     </div>
                 </div>
             </section>
-    */}
+      */}
 
-            <Footer />
-        </div>
-    )
+      <Footer />
+    </div>
+  );
 }
