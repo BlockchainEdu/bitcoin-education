3e7fac7db744f131781ce38be90eb96874fa37f6
Erick Pinos <erick@erickpinos.com>
2023-06-03 23:38:02 -0400

    added reader icons to subscribe page

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -42,7 +42,74 @@ export default function Subscribe() {
                 <iframe src="https://embeds.beehiiv.com/cfab9b0e-aa74-4e4d-bf81-2a81e1904f6c?slim=true" data-test-id="beehiiv-embed" height="52" frameBorder="0" scrolling="no" style={{ margin: "0", borderRadius: "0px", backgroundColor: "transparent", width: "100%" }}></iframe>
               </div>
             </div>
+
+            <div className="col-span-2 mt-12 text-center">
+              <div className="flex justify-center">
+                <div className="w-full grid grid-cols-8 gap-9 relative" style={{ margin: "-10px", width: "calc(20px * 8 + 4px * 7)" }}>
+                  <div className="rounded-full bg-gray-300 w-12 h-12 border-benorange border-2">
+                    <img
+                      src="/images/stories/priya-ganguly.jpeg"
+                      alt="User Image 1"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="rounded-full bg-gray-300 w-12 h-12 border-benorange border-2 -z-1">
+                    <img
+                      src="/images/stories/valkyrie-holmes.jpg"
+                      alt="User Image 2"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="rounded-full bg-gray-300 w-12 h-12 border-benorange border-2 -z-2">
+                    <img
+                      src="/images/stories/sohail-mohammed.jpg"
+                      alt="User Image 2"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="rounded-full bg-gray-300 w-12 h-12 border-benorange border-2 -z-3">
+                    <img
+                      src="/images/stories/sarah-roff.jpeg"
+                      alt="User Image 3"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="rounded-full bg-gray-300 w-12 h-12 border-benorange border-2 -z-4">
+                    <img
+                      src="/images/stories/anthony-ung.jpeg"
+                      alt="User Image 4"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="rounded-full bg-gray-300 w-12 h-12 border-benorange border-2 -z-5">
+                    <img
+                      src="/images/stories/roberto-martinez.jpeg"
+                      alt="User Image 5"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="rounded-full bg-gray-300 w-12 h-12 border-benorange border-2 -z-6">
+                    <img
+                      src="/images/stories/amrita-bhasin.jpeg"
+                      alt="User Image 6"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="rounded-full bg-benorange-500 w-12 h-12 border-benorange border-2 -z-7">
+                    <img
+                      src="/images/stories//drew-cousin.jpeg"
+                      alt="User Image 6"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </div>
+                </div>
+              </div>
+              <p className="mt-2 ml-8 text-bengrey-500 text-center text-md">Join thousands of readers!</p>
+            </div>
           </div>
+
+
+
 
           <div className="col-span-1">
             <a href="https://beats.blockchainedu.org">
