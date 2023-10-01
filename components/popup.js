0a76dcfabc426358e64fa7c2e7385fe0db676b01
Antonio <antonio@gomes.ai>
2023-10-01 15:42:20 +0100

    Pop-up removed and sponsor page updated

--- components/popup.js
+++ components/popup.js
@@ -17,13 +17,13 @@ export default function Popup() {
 
   return (
     <>
-      {display && (
+      {false && (
         <div id="popup" className="overlay-popup-container" onClick={closePopup}>
           <div className="overlay-popup mx-6" onClick={(e) => e.stopPropagation()}>
 
             <div className="flex justify-end">
               <div className="overlay-popup-close w-6 mb-3" onClick={closePopup}><img src="/images/popup-close.svg" /></div>
-            </div>
+            </div> */
 
             <div className="flex flex-col lg:flex-row bg-white gap-x-20 px-6 lg:px-0">
               <div className="w-full lg:w-1/2 py-10 pl-0 lg:ml-14">
@@ -37,16 +37,17 @@ export default function Popup() {
                   Learn why crypto crashed in 2022 and how to prepare for 2024 👇
                 </div>
                 <div className="flex justify-center">
-                  <iframe src="https://embeds.beehiiv.com/cfab9b0e-aa74-4e4d-bf81-2a81e1904f6c?slim=true&utm_source=website&utm_medium=popup&utm_content=popup" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style={{margin: "0", borderRadius: "0px", backgroundColor: "transparent", width: "100%"}}></iframe>
+                  <iframe src="https://embeds.beehiiv.com/cfab9b0e-aa74-4e4d-bf81-2a81e1904f6c?slim=true&utm_source=website&utm_medium=popup&utm_content=popup" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style={{ margin: "0", borderRadius: "0px", backgroundColor: "transparent", width: "100%" }}></iframe>
                 </div>
               </div>
-              <div className="w-full lg:h-auto lg:w-1/3" style={{ backgroundImage: "url(/images/your-guide-to-the-wild-world-of-crypto-3.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover"}}>
+              <div className="w-full lg:h-auto lg:w-1/3" style={{ backgroundImage: "url(/images/your-guide-to-the-wild-world-of-crypto-3.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover" }}>
               </div>
             </div>
 
-          </div>
-        </div>
-      )}
+          </div >
+        </div >
+      )
+      }
     </>
   );
 }
--- pages/sponsor.js
+++ pages/sponsor.js
@@ -14,77 +14,78 @@ export default function Sponsor() {
 
       <section className="pt-10 lg:py-10 lg:pb-0 px-0">
 
-        <div className="w-11/12 lg:w-7/12 mx-auto grid grid-cols-1 lg:grid-cols-1 gap-10" style={{ maxWidth: "1200px"}}>
+        <div className="w-11/12 lg:w-7/12 mx-auto grid grid-cols-1 lg:grid-cols-1 gap-10" style={{ maxWidth: "1200px" }}>
 
 
-            <h2 className="text-3xl font-bold text-center">Advertise with us</h2>
-            <p>BEN Beats is one of the fastest growing crypto newsletters in the world. We spend hundreds of hours sourcing the best crypto news, events, jobs, and tools and distill it down into a <a href="beats.blockchainedu.org">4 minute newsletter every week</a></p>
-            <a href="https://bit.ly/ben-beats-deck" target="_blank"><div>See full deck                                <span>
-                                    <Image
-                                        width="24px"
-                                        height="24px"
-                                        src="/images/home-arrow.svg"
-                                    />
-                                </span></div></a>
-                    <div className="flex justify-center">
+          <h2 className="text-3xl font-bold text-center">Advertise with us</h2>
+          <p>BEN Beats is one of the fastest growing crypto newsletters in the world. We spend hundreds of hours sourcing the best crypto news, events, jobs, and tools and distill it down into a <a href="beats.blockchainedu.org">4 minute newsletter every week</a></p>
+          <a href="https://bit.ly/ben-beats-deck" target="_blank"><div>See full deck                                <span>
+            <Image
+              width="24px"
+              height="24px"
+              src="/images/home-arrow.svg"
+            />
+          </span></div></a>
+          <div className="flex justify-center">
 
-                      <a href="https://calendly.com/blockchainedu/main/" className="px-4 py-2 bg-benorange-500 text-white rounded-md">
-                        Book Main Ad
-                      </a>
-                      </div>
+            <a href="mailto:contact@blockchainedu.org" className="px-4 py-2 bg-benorange-500 text-white rounded-md">
+              Book Main Ad
+            </a>
+          </div>
 
-             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
-                <div className="bg-white rounded-lg shadow p-6">
-                    <p><b>📣 Reach</b>: 7.4k+ subscribers, 50k+ social media followers.</p>
-                </div>
 
-                <div className="bg-white rounded-lg shadow p-6">
-                    <p><b>🖱️ Open rate</b>: 31.1%</p>
-                </div>
-
-                <div className="bg-white rounded-lg shadow p-6">
-                    <p><b>👀 Average Impressions</b>: 6,993+ per post across newsletter and socials</p>
-                </div>
-
-                <div className="bg-white rounded-lg shadow p-6">
-                    <p><b>🌎 Location</b>: Our readers are mainly based in the US & Canada</p>
-                </div>
-
-                <div className="bg-white rounded-lg shadow p-6">
-                    <p><b>👥 Audience</b>: Our readers are mainly recent graduates in the blockchain space and are a mix of developers, builders and traders. Many are current students and looking to work full-time in the crypto space</p>
-                </div>
-
-                <div className="bg-white rounded-lg shadow p-6">
-                    <p><b>🚀 Growth</b>: 300+ newsletter subscribers in the past month and 1k+ Twitter followers</p>
-                </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+            <div className="bg-white rounded-lg shadow p-6">
+              <p><b>📣 Reach</b>: 7.4k+ subscribers, 50k+ social media followers.</p>
             </div>
 
-             <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-8">
-                <div className="bg-white rounded-lg shadow p-6">
-                            <h3 className="text-xl font-bold mb-4 text-center">Main Ad</h3>
-                    <div className="mb-1">Placement near the top of the email. 400 characters. 1 spot available per issue. $500 per spot</div>
-                    <div className="mb-1">Included a dedicated social media post across Twitter, Instagram, Facebook, LinkedIn, TikTok, and YouTube</div>
-                    <div><b>Bundle</b>: Get a 15% discount for purchasing 4 spots.</div>
-                    <div className="flex justify-center mt-4 mb-4">
-                      <a href="https://calendly.com/blockchainedu/main/" className="px-4 py-2 bg-benorange-500 text-white rounded-md">
-                        Book Main Ad
-                      </a>
-                    </div>
-                </div>
-             </div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <p><b>🖱️ Open rate</b>: 31.1%</p>
+            </div>
 
-             <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-8">
-                <div className="bg-white rounded-lg shadow p-6">
-                            <h3 className="text-xl font-bold mb-4 text-center">Example</h3>
-                        <a href="https://calendly.com/blockchainedu/main/"><img src="images/web3-daily-ad.png" alt="Web3 Daily Ad"></img></a>
-                </div>
-             </div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <p><b>👀 Average Impressions</b>: 6,993+ per post across newsletter and socials</p>
+            </div>
 
-            <div className="mb-10"><i>All ads are subject to approval. The best fit for our audience are products related to blockchain, tech, and productivity. Irrelevant ads will not be approved.</i></div>
+            <div className="bg-white rounded-lg shadow p-6">
+              <p><b>🌎 Location</b>: Our readers are mainly based in the US & Canada</p>
+            </div>
+
+            <div className="bg-white rounded-lg shadow p-6">
+              <p><b>👥 Audience</b>: Our readers are mainly recent graduates in the blockchain space and are a mix of developers, builders and traders. Many are current students and looking to work full-time in the crypto space</p>
+            </div>
+
+            <div className="bg-white rounded-lg shadow p-6">
+              <p><b>🚀 Growth</b>: 300+ newsletter subscribers in the past month and 1k+ Twitter followers</p>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-8">
+            <div className="bg-white rounded-lg shadow p-6">
+              <h3 className="text-xl font-bold mb-4 text-center">Main Ad</h3>
+              <div className="mb-1">Placement near the top of the email. 400 characters. 1 spot available per issue. $500 per spot</div>
+              <div className="mb-1">Included a dedicated social media post across Twitter, Instagram, Facebook, LinkedIn, TikTok, and YouTube</div>
+              <div><b>Bundle</b>: Get a 15% discount for purchasing 4 spots.</div>
+              <div className="flex justify-center mt-4 mb-4">
+                <a href="mailto:contact@blockchainedu.org" className="px-4 py-2 bg-benorange-500 text-white rounded-md">
+                  Book Main Ad
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-8">
+            <div className="bg-white rounded-lg shadow p-6">
+              <h3 className="text-xl font-bold mb-4 text-center">Example</h3>
+              <a href="mailto:contact@blockchainedu.org"><img src="images/web3-daily-ad.png" alt="Web3 Daily Ad"></img></a>
+            </div>
+          </div>
+
+          <div className="mb-10"><i>All ads are subject to approval. The best fit for our audience are products related to blockchain, tech, and productivity. Irrelevant ads will not be approved.</i></div>
         </div>
 
       </section>
       <Footer />
-        </div>
-    )
+    </div>
+  )
 }
