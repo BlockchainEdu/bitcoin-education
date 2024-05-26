8cde17d2171087eab21a0d44ba233516963c1b51
Filipe Ines <filipe.jl.ines@gmail.com>
2024-05-26 19:42:12 +0100

    add internship page

---
+++ pages/internship.js
@@ -0,0 +1,20 @@
+import Footer from '../components/footer';
+import HeaderWithLogoDark from '../components/headerWithLogoDark';
+import Head from "next/head"
+
+export default function Contact() {
+    return (
+        <div id="contact-page">
+            <HeaderWithLogoDark />
+            <Head>
+                <title>Internship | Blockchain Education Network</title>
+            </Head>
+            <section className="pt-10 lg:pt-24 py-24 rounded-md mt-4 lg:mt-10">
+                <div className="max-w-4xl m-auto">
+                    <iframe src="https://forms.monday.com/forms/embed/93ba212497018932d89afe673c7a35ee?r=use1" className="m-auto" width="100%" style={{height:"90vh"}}></iframe>
+                </div>
+            </section>
+            <Footer />
+        </div>
+    )
+}
