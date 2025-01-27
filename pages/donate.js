cb13e3ff80c65dec2667843c625a8c313d686656
gabdealmeida <dealmeida.gabriela@outlook.com>
2025-01-27 22:47:40 +0000

    fix text padding left

--- pages/donate.js
+++ pages/donate.js
@@ -330,7 +330,7 @@ export default function Donate() {
       </section>
       <section className="bg-gray-50">
         <div className="max-w-7xl m-auto py-24">
-          <h3 className="text-4xl md:text-5xl text-center lg:text-left font-black text-black max-w-5xl pb-14 leading-snug">
+          <h3 className="text-4xl md:text-5xl text-center lg:text-left font-black text-black max-w-5xl pb-14 leading-snug pl-7">
             Frequently Asked Questions
           </h3>
           <div className="px-7 flex flex-col lg:flex-row justify-between">
