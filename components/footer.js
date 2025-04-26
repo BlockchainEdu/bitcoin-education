92a153a565cd1ad8dfdfde46b76e885361c76831
Antonio <antonio@gomes.ai>
2025-04-26 20:39:34 +0100

    updated learn link

--- components/footer.js
+++ components/footer.js
@@ -121,7 +121,7 @@ export default function Footer() {
                   </a>
                 </li>
                 <li className="text-sm">
-                  <a href="https://twitter.com/blockchainedu" target="_blank">
+                  <a href="https://www.chainstories.xyz/" target="_blank">
                     Start Learning
                   </a>
                 </li>
