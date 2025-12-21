38fe640f6c47b5e65ee3cf0179f0d44a84685152
Gabriela <gabriela.coelho.ext@spms.min-saude.pt>
2025-12-21 22:50:21 +0000

    Add static redirect for /podcast to chainstories.xyz

--- pages/ibiza.js
+++ pages/ibiza.js
@@ -1,6 +1,5 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
-import StandardButton from "../components/standardButton";
 import Head from "next/head";
 import ImageSlider from "../components/imageSlider";
 
@@ -39,9 +38,14 @@ export default function IbizaHackerHouse() {
             An Exclusive Mastermind Retreat for Web3’s Top Founders
           </p>
           <div className="mt-6">
-            <button className="bg-benorange-500 text-white font-bold py-3 px-6 rounded-full hover:bg-benorange-400 transition">
+            <a
+              href="https://t.me/cryptoniooo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-benorange-500 text-white font-bold py-3 px-6 rounded-full hover:bg-benorange-400 transition inline-block"
+            >
               Apply Now
-            </button>
+            </a>
           </div>
         </div>
       </section>
@@ -60,7 +64,9 @@ export default function IbizaHackerHouse() {
           </p>
           <div className="flex justify-center mt-6">
             <a
-              href="#apply"
+              href="https://t.me/cryptoniooo"
+              target="_blank"
+              rel="noopener noreferrer"
               className="px-6 py-3 bg-benorange-500 text-white rounded-full"
             >
               Apply Now
@@ -93,7 +99,9 @@ export default function IbizaHackerHouse() {
             </ul>
             <div className="flex justify-center lg:justify-start mt-6">
               <a
-                href="#apply"
+                href="https://t.me/cryptoniooo"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="px-6 py-3 bg-white text-benorange-500 rounded-full"
               >
                 Apply Now
@@ -153,13 +161,13 @@ export default function IbizaHackerHouse() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="h-8 w-8"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
                   />
                 </svg>
@@ -180,13 +188,13 @@ export default function IbizaHackerHouse() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="h-8 w-8"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
                   />
                 </svg>
@@ -241,7 +249,9 @@ export default function IbizaHackerHouse() {
           </p>
           <div className="flex justify-center mt-6">
             <a
-              href="#"
+              href="https://t.me/cryptoniooo"
+              target="_blank"
+              rel="noopener noreferrer"
               className="px-6 py-3 bg-benorange-500 text-white rounded-full"
             >
               Apply Now
